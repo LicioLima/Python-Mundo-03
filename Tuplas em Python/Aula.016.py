c7a9548fa7092c6f9727b6d5eefc44824f544647
Lício Lima <liciomlima@gmail.com>
2022-04-18 16:41:00 -0300

    Continuidade aos estudos em python

---
+++ Tuplas em Python/Aula.016.py
@@ -0,0 +1,9 @@
+# TUPLAS SÃO IMUTÁVEIS!!
+'''a = (2, 4, 6, 8)
+b = (1, 3, 5, 7)
+c = a + b
+print(c)
+print(c.index(6))''' #Exemplo de tupla
+
+pessoa = ('Lício', 32, 'M', 71.2)
+del
---
+++ Tuplas em Python/Ex.72_Número_Por_Extenso.py
@@ -0,0 +1,4 @@
+numero = len(20)
+for numero in range(len(20)):
+    print(f'Digite seu {numero}')
+print('Qual o seu número?')
