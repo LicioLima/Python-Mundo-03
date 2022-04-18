8149a5637437b4fee345c95c2a172ce266dfcf11
Lício Lima <liciomlima@gmail.com>
2022-04-18 16:58:37 -0300

    Primeiro exercício

--- Tuplas em Python/Ex.72_Número_Por_Extenso.py
+++ Tuplas em Python/Ex.72_Número_Por_Extenso.py
@@ -1,4 +1,9 @@
-numero = len(20)
-for numero in range(len(20)):
-    print(f'Digite seu {numero}')
-print('Qual o seu número?')
+cont = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez',
+        'onze', 'doze', 'treze', 'catorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
+
+while True:
+    num = int(input('Digite um numero de 0 a 20: '))
+    if 0 <= num <= 20:
+        break
+    print('Tenho novamente.', end='')
+print(f'Você digitou o numero {cont[num]}')
